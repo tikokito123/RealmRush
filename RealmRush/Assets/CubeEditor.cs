4f68f2928d1f1b7b9ce51225cbbdc73b832852e2
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-07-25 05:26:36 +0300

    cubes and world!

---
+++ RealmRush/Assets/CubeEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    TextMesh textMesh;
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
