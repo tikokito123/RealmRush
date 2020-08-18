f4114f5a221e52a9477086b22ffdc908529ae9b3
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-08-18 21:23:16 +0300

    enemies can die!

---
+++ RealmRush/Assets/EnemyDeath.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour
+{
+    [SerializeField] GameObject deathFx;
+    public int health = 3;
+    void OnParticleCollision(GameObject other)
+    {
+        Death();
+    }
+
+    private void Death()
+    {
+        
+        if (health <= 0)
+        {
+            Instantiate(deathFx, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        health--;
+    }
+}
