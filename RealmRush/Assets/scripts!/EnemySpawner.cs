63048e35d9f33cf02a176afb37e0c134dc22e42c
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-08-19 12:41:47 +0300

    coroutine by spawning enemies!

---
+++ RealmRush/Assets/scripts!/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawn = 3f;
+    [SerializeField] GameObject enemy;
+    private void Start()
+    {
+        StartCoroutine(EnemySpawn());
+    }
+    IEnumerator EnemySpawn()
+    {
+        while (true) // forever!
+        {
+            Instantiate(enemy,new Vector3(0,0,0), Quaternion.identity);
+            yield return new WaitForSeconds(secondsBetweenSpawn);        
+        }
+    }
+}
